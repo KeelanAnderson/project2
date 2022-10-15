9b3c38d64248716a8041869f7669829e12207a83
KeelanAnderson <keelan.anderson.ka@gmail.com>
2022-10-15 14:16:16 +0000

    add some style and basic html

---
+++ assets/js/script.js
@@ -0,0 +1,122 @@
+function startGame()
+
+function setNextQuestion()
+
+function selectAnswer()
+
+const questions = [
+
+    {
+        "question": "Along with Oxygen, which element is primarily responsible for the sky appearing blue?",
+        "correct_answer": "Nitrogen",
+        "incorrect_answers": ["Helium", "Carbon", "Hydrogen"]
+    },
+
+    {
+       
+        "question": "In aeronautics, flaps and slats are used to control what on an aircraft?",
+        "correct_answer": "Lift",
+        "incorrect_answers": ["Thrust", "Drag", "Weight "]
+    },
+    {
+       
+        "question": "What mineral has the lowest number on the Mohs scale?",
+        "correct_answer": "Talc",
+        "incorrect_answers": ["Quartz", "Diamond", "Gypsum"]
+    },
+    {
+       
+        "question": "Gannymede is the largest moon of which planet?",
+        "correct_answer": "Jupiter",
+        "incorrect_answers": ["Uranus", "Neptune", "Mars"]
+    },
+    {
+       
+        "question": "To the nearest minute, how long does it take for light to travel from the Sun to the Earth?",
+        "correct_answer": "8 Minutes",
+        "incorrect_answers": ["6 Minutes", "2 Minutes", "12 Minutes"]
+    },
+    {
+       
+        "question": "What is the chemical formula for ammonia?",
+        "correct_answer": "NH3",
+        "incorrect_answers": ["CO2", "NO3", "CH4"]
+    },
+    {
+       
+        "question": "What is the atomic number of the element Strontium?",
+        "correct_answer": "38",
+        "incorrect_answers": ["73", "47", "11"]
+    },
+    {
+      
+        "question": "Which part of the body does glaucoma affect?",
+        "correct_answer": "Eyes",
+        "incorrect_answers": ["Throat", "Stomach", "Blood"]
+    },
+    {
+        
+        "question": "What term is best associated with Sigmund Freud?",
+        "correct_answer": "Psychoanalysis",
+        "incorrect_answers": ["Cognitive-Behavioral Therapy", "Theory of Gravity", "Dialectical Behavior Therapy"]
+    },
+    {
+       
+        "question": "After which Danish city is the 72th element on the periodic table named?",
+        "correct_answer": "Copenhagen",
+        "incorrect_answers": ["Odense", "Herning", "Skagen"]
+    },
+    {
+        
+        "question": "Which of these is a type of stretch\/deep tendon reflex?",
+        "correct_answer": "Ankle jerk reflex",
+        "incorrect_answers": ["Gag reflex", "Pupillary light reflex", "Scratch reflex"]
+    },
+    {
+        
+        "question": "The human right lung has how many lobes?",
+        "correct_answer": "3",
+        "incorrect_answers": ["4", "2", "1"]
+    },
+    {
+        "question": "Myopia is the scientific term for which condition?",
+        "correct_answer": "Shortsightedness",
+        "incorrect_answers": ["Farsightedness", "Double Vision", "Clouded Vision"]
+    },
+    {
+      
+        "question": "In Chemistry, how many isomers does Butanol (C4H9OH) have?",
+        "correct_answer": "4",
+        "incorrect_answers": ["3", "5", "6"]
+    },
+    {
+      
+        "question": "What are human nails made of?",
+        "correct_answer": "Keratin",
+        "incorrect_answers": ["Bone", "Chitin", "Calcium"]
+    },
+    {
+       
+        "question": "Approximately how long is a year on Uranus?",
+        "correct_answer": "84 Earth years",
+        "incorrect_answers": ["47 Earth years", "62 Earth years", "109 Earth years"]
+    },
+    {
+        
+        "question": "Which of the following men does not have a chemical element named after him?",
+        "correct_answer": "Sir Isaac Newton",
+        "incorrect_answers": ["Albert Einstein", "Niels Bohr", "Enrico Fermi"]
+    },
+    {
+        
+        "question": "What is the half-life of Uranium-235?",
+        "correct_answer": "703,800,000 years",
+        "incorrect_answers": ["4,300,400,000 years", "1,260,900,000 years", "Uranium-235 is a stable isotope"]
+    },
+    
+    {
+        "question": "How old is the universe?",
+        "correct_answer": "13.8 Billion Years",
+        "incorrect_answers": ["4.5 Billion Years", "7.9 Billion Years", "16.2 Billion Years"]
+    }
+]
