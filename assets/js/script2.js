6c42a2d4678d4496f200b7e83071bc8fb15766b0
KeelanAnderson <keelan.anderson.ka@gmail.com>
2022-10-20 14:56:13 +0000

    add js to display questions and answers for each question

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,37 +1,92 @@
-const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
-const answerButtons = document.getElementById('answer-buttons');
-const questionContainer = document.getElementById('question-container')
+const answerButtonsElement = document.getElementById('answer-buttons');
 const questionElement = document.getElementById('questions')
+const startButton = document.getElementById('start-btn');
+const questionContainer = document.getElementById('question-container')
+
+let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener('click', runGame)
 
-
 function runGame() {
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+    currentQuestionIndex = 0
+    questionContainer.classList.remove('hide')
     displayNextQuestion()
 };
 
+
 function displayNextQuestion() {
-    questionContainer.classList.remove('hide')
-  
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
 
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
 
+    }
+}
 
+function selectAnswer(e) {
+     
 
-const questions = [
+}
 
-    {
-        question: "Along with Oxygen, which element is primarily responsible for the sky appearing blue?",
-        correct_answer: "Nitrogen",
-        incorrect_answers: ["Helium", "Carbon", "Hydrogen"]
+let questions = [{
+        question: "How old is the universe?",
+        answers: [{
+                text: "13.8 Billion Years",
+                correct: true
+            },
+            {
+                text: "2022 Years",
+                correct: false
+            },
+            {
+                text: "900 Trillion Years",
+                correct: false
+            },
+            {
+                text: "60 Million Years",
+                correct: false
+            },
+        ]
     },
     {
-
-        question: "In aeronautics, flaps and slats are used to control what on an aircraft?",
-        correct_answer: "Lift",
-        incorrect_answers: ["Thrust", "Drag", "Weight "]
+        question: "How old am i?",
+        answers: [{
+                text: "7 Years",
+                correct: true
+            },
+            {
+                text: "2022 Years",
+                correct: false
+            },
+            {
+                text: "21 Years",
+                correct: false
+            },
+            {
+                text: "3",
+                correct: false
+            },
+        ]
     }
 ]
